591e4714e278ee5a6cbdf3c23ae2a33fe2bb3dc1
henrymaplehonda <hengyanphotography@gmail.com>
2026-08-03 23:03:11 -0400

    Correct Firebase web API key

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,12 @@ window.PEI_FIREBASE_SYNC = {
   enabled: true,
   tripId: 'pei-2026',
   firebaseConfig: {
-    apiKey: 'AIzaSyDbyTn8lWXUEiNq_x_Zzq56lIi0YSmKA5w',
+    apiKey: 'AIzaSyDbYtn8lWXUEiNq_x_Zzq56lIiOYSmKA5w',
     authDomain: 'pei-road-trip-sync.firebaseapp.com',
     projectId: 'pei-road-trip-sync',
     storageBucket: 'pei-road-trip-sync.firebasestorage.app',
     messagingSenderId: '319172064576',
-    appId: '1:319172064576:web:5a6237a144c30c5f16b6a5'
+    appId: '1:319172064576:web:5a6237a144c30c5f16b6a5',
+    measurementId: 'G-NNVYDNDCCE'
   }
 };
