8ba853a1a8806de7084d66f2caf94970a6ff0bb6
henrymaplehonda <hengyanphotography@gmail.com>
2026-08-03 22:27:45 -0400

    Add Firebase sync configuration template

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.PEI_FIREBASE_SYNC = {
+  enabled: false,
+  tripId: 'pei-2026',
+  firebaseConfig: {
+    apiKey: 'PASTE_API_KEY',
+    authDomain: 'PASTE_PROJECT_ID.firebaseapp.com',
+    projectId: 'PASTE_PROJECT_ID',
+    storageBucket: 'PASTE_PROJECT_ID.firebasestorage.app',
+    messagingSenderId: 'PASTE_MESSAGING_SENDER_ID',
+    appId: 'PASTE_APP_ID'
+  }
+};
